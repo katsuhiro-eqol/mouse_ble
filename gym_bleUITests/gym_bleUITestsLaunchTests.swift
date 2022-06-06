8626dc49f27b12c45eb99dabcdc869e94255b3a4
tjhgg443 <katsuhiro.yamashita.sr01@gmail.com>
2022-06-06 14:44:33 +0900

    Initial Commit

---
+++ gym_bleUITests/gym_bleUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  gym_bleUITestsLaunchTests.swift
+//  gym_bleUITests
+//
+//  Created by 山下克宏 on 2022/06/06.
+//
+
+import XCTest
+
+class gym_bleUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
